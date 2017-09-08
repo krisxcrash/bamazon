803ce17480b94c8d53bcd65233d536e5e6a323e2
Kristine Martin <kristinemartin@kristines-mbp.attlocal.net>
2017-09-07 23:05:17 -0700

    added inquirer prompts

---
+++ bamazonCustomer.js
@@ -0,0 +1,56 @@
+var mysql = require("mysql");
+var inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+  
+    // Your username
+    user: "uci.projects",
+    
+    // Your password
+    password: "uciprojects123",
+    database: "bamazon_DB"
+  });
+
+  connection.connect(function(err) {
+      if (err) throw err;
+      runItems();
+  });
+
+  function runItems() {
+    inquirer
+        .prompt([
+            {
+                name: "product_id",
+                type: "input",
+                message: "What is the ID of the product you would like to buy?"
+            },
+            {
+                name: "quantity",
+                type: "input",
+                message: "How many units would you like to buy?",
+                validate: function(value) {
+                    if (isNaN(value) === false) {
+                        return true;
+                        updateQuantity();
+                    }
+                    return false;
+                    }
+            }
+        ])
+  };
+
+function updateQuantity() {
+    var query = connection.query(
+        "UPDATE stock_quantity SET ? WHERE ?",
+        [
+            {
+                stock_quantity: -1
+            }, 
+            {
+                product_id: test//user input
+            }
+        ]
+    )
+}
---
+++ schema.sql
@@ -0,0 +1,14 @@
+CREATE database bamazon_DB;
+
+USE bamazon_DB;
+
+CREATE TABLE products (
+    item_id INT NOT NULL AUTO_INCREMENT,
+    product_name VARCHAR(100) NOT NULL,
+    department_name VARCHAR(100) NOT NULL,
+    price INT default 0,
+    stock_quantity INT default 0,
+    PRIMARY KEY (item_id)
+);
+
+SELECT * FROM products;
